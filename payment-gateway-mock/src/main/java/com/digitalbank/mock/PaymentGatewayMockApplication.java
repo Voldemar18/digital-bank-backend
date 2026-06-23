9205688973fd4a7edf0f64247894acdfe0d0682d
alowe17 <p.danil51007@gmail.com>
2026-06-23 23:30:10 +0300

    init: Инициализация структуры микросервисов банка

---
+++ payment-gateway-mock/src/main/java/com/digitalbank/mock/PaymentGatewayMockApplication.java
@@ -0,0 +1,13 @@
+package com.digitalbank.mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentGatewayMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentGatewayMockApplication.class, args);
+	}
+
+}
---
+++ payment-gateway-mock/src/test/java/com/digitalbank/mock/PaymentGatewayMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalbank.mock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentGatewayMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
